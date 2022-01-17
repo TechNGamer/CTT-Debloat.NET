c2c30532b4e6ed8cd35de6153ba057c4af7d89a5
Jeff Crow <jeffwcrow@gmail.com>
2022-01-16 21:00:22 -0600

    Added a new struct that represents each object in the dictionary of the JSON.

---
+++ CTTDebloatNET/Models/ProgramInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CTTDebloatNET.Models {
+	public readonly struct ProgramInfo {
+		public string DisplayName { get; }
+
+		public string[] IDs { get; }
+
+		[JsonConstructor]
+		public ProgramInfo( [JsonProperty("displayName")]string name, [JsonProperty("id")]string[] ids ) {
+			if ( name == null ) {
+				throw new ArgumentNullException( nameof( name ), "Expected a name, but got null." );
+			}
+
+			if ( string.IsNullOrWhiteSpace( name ) ) {
+				throw new ArgumentException( "There must be a name associated with the ids.", nameof( name ) );
+			}
+
+			if ( ids == null ) {
+				throw new ArgumentNullException( nameof( ids ), "Expected a list of IDs." );
+			}
+
+			if ( ids.Length == 0 ) {
+				throw new ArgumentOutOfRangeException( nameof( ids ), "There must be at least 1 ID to associate with the program." );
+			}
+
+			DisplayName = name;
+			IDs         = ids;
+		}
+
+		public override string ToString() {
+			var builder = new StringBuilder( "Name:\t" ).AppendLine( DisplayName );
+
+			for ( var i = 0; i < IDs.Length; ++i ) {
+				builder.Append( "ID [" ).Append( i + 1 ).Append( "]:\t" ).AppendLine( IDs[i] );
+			}
+
+			return builder.ToString();
+		}
+	}
+}
