85022be91d94f99523a53595a6f935a9e2a1e6e8
Jeff Crow <jeffwcrow@gmail.com>
2022-01-17 21:09:53 -0600

    Made a View for the ProgramInfo struct.

---
+++ CTTDebloatNET/Views/ProgramInstallOptionView.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CTTDebloatNET.Views {
+	public class ProgramInstallOptionView : UserControl {
+		public ProgramInstallOptionView() {
+			InitializeComponent();
+		}
+
+		private void InitializeComponent() {
+			AvaloniaXamlLoader.Load( this );
+		}
+	}
+}
