4c7ec1cfddbcf497c5a292f723e0455cecf8cf1a
Jeff Crow <jeffwcrow@gmail.com>
2022-01-17 21:10:13 -0600

    Added a ViewModel that will hook up to the View for the ProgramInfo.

---
+++ CTTDebloatNET/ViewModels/ProgramInstallOptionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CTTDebloatNET.Models;
+
+namespace CTTDebloatNET.ViewModels {
+	public class ProgramInstallOptionViewModel : ViewModelBase {
+
+		public string ProgramName => Program.DisplayName;
+
+		public ProgramInfo Program {
+			get;
+		}
+
+		public ProgramInstallOptionViewModel( ProgramInfo info ) {
+			Program = info;
+		}
+		
+	}
+}
